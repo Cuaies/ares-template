3c11b9e5add4ba7d608680b64734786c076ce63e
Cuaies <61624423+Cuaies@users.noreply.github.com>
2023-08-10 17:40:35 +0300

    feat(commands): create chat input command class

---
+++ src/modules/commands/chatInputCommand.ts
@@ -0,0 +1,40 @@
+import { Mixin } from "ts-mixer";
+import { AresBaseCommandData } from "./baseCommand";
+import {
+  ApplicationCommandType,
+  CommandInteraction,
+  SlashCommandBuilder,
+} from "discord.js";
+
+/**
+ * Represents a chat input application command builder.
+ */
+export class AresChatInputCommandBuilder extends Mixin(
+  AresBaseCommandData,
+  SlashCommandBuilder
+) {
+  type = ApplicationCommandType.ChatInput;
+}
+
+/**
+ * Represents a chat input application command.
+ */
+export class AresChatInputCommand {
+  /**
+   * Command data.
+   */
+  readonly data;
+
+  /**
+   * Command function meant to execute once an appropriate interaction is received.
+   */
+  readonly execute;
+
+  constructor(
+    data: AresChatInputCommandBuilder,
+    execute: (interaction: CommandInteraction) => Promise<void>
+  ) {
+    this.data = data;
+    this.execute = execute;
+  }
+}
