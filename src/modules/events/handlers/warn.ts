c3dd6f7368a38e5fcd35e48b7c003f3a4fdb9710
Cuaies <61624423+Cuaies@users.noreply.github.com>
2023-09-22 18:00:50 +0300

    build(gitignore): ignore handler data

--- src/modules/events/handlers/warn.ts
+++
@@ -1,8 +0,0 @@
-import { Events } from "discord.js";
-import AresEventHandler from "../handler";
-import { logger } from "../../logger/logger";
-import { LogMessagesCodes, LogScopes } from "../../../ts/enums";
-
-export default new AresEventHandler(Events.Warn, false, true, (message) => {
-  logger.log(LogScopes.Client, LogMessagesCodes.StringLogWarn, message);
-});
