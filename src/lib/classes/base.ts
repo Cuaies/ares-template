686b90abce7632402b557f0dd40aa553ef7f9c2a
Cuaies <61624423+Cuaies@users.noreply.github.com>
2023-12-07 00:19:20 +0100

    refactor(AresBase): restructure class

--- src/lib/classes/base.ts
+++ src/lib/classes/base.ts
@@ -1,13 +1,19 @@
 import { isProduction } from "../../utils/helpers";
 
 /**
- * The foundation of all classes.
+ * Represents the base class.
+ *
+ * Provides common functionality and properties for classes.
  */
 export class AresBase {
   /**
-   * Defines the state of the process, whether it's running in production mode or not.
+   * Indicates whether the application is running in production mode.
+   *
+   * @returns `true` if the application is running in production mode; otherwise, `false`.
    */
-  readonly isProduction: boolean = isProduction();
+  public get isProduction(): boolean {
+    return isProduction();
+  }
 
   // eslint-disable-next-line @typescript-eslint/no-empty-function
   protected constructor() {}
