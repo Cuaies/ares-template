d7ee216ecd8943e3a519b49e7c4de001c6742701
Cuaies <61624423+Cuaies@users.noreply.github.com>
2023-12-07 02:19:11 +0100

    refactor(AresBaseManager): restructure class

--- src/lib/classes/baseManager.ts
+++ src/lib/classes/baseManager.ts
@@ -2,22 +2,48 @@ import { LogScopes } from "../../ts/enums";
 import { AresBaseModule } from "./baseModule";
 import { AresResults } from "./results";
 import { AresClient } from "./client";
+import { AresManagerOptions } from "../../ts/types";
+import { Awaitable } from "discord.js";
+import { types } from "util";
 
 /**
- * The foundation of all managers.
+ * Abstract base class for managers.
  */
 export abstract class AresBaseManager extends AresBaseModule {
   /**
-   * Manager's results class.
+   * Results instance associated with the manager.
    */
-  abstract readonly results: InstanceType<typeof AresResults>;
+  readonly results: InstanceType<typeof AresResults> = new AresResults(
+    this.scope
+  );
 
   protected constructor(client: AresClient, scope: LogScopes) {
     super(client, scope);
   }
 
   /**
-   * Initializes the manager, loading the data and getting it in the ready state.
+   * Sets up the manager by supplying essential data and performing preparatory tasks.
+   *
+   * @remarks
+   * Orchestrates the initialization process, encompassing the setup of
+   * foundational data and execution of preparatory tasks. This ensures that the manager is equipped to
+   * handle interactions and various operations effectively.
    */
-  abstract init(): Promise<void>;
+  abstract setup(opts: AresManagerOptions): Awaitable<void>;
+
+  /**
+   * Initializes the manager.
+   *
+   * @remarks
+   * This method should be called after creating an instance of the class to perform any necessary setup or initialization.
+   */
+  public async init(opts: AresManagerOptions): Promise<void> {
+    if (types.isAsyncFunction(this.setup)) {
+      await this.setup(opts);
+    } else {
+      this.setup(opts);
+    }
+
+    this.results.display();
+  }
 }
--- src/ts/types.ts
+++ src/ts/types.ts
@@ -2,3 +2,18 @@ import { AresChatInputCommand } from "../modules/commands/chatInputCommand";
 
 export type ResultsStatus = { success: boolean; ok: "ok" | "non-ok" };
 export type AresApplicationCommandType = AresChatInputCommand;
+
+/**
+ * Manager options.
+ */
+export type AresManagerOptions = {
+  /**
+   * Loader configuration.
+   */
+  loader: {
+    /**
+     * The path to the directory containing manager data.
+     */
+    dirPath: string;
+  };
+};
